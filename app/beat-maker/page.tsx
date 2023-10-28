67e7b440909dd8334678565f0d06aea1eac546af
Tobias112358 <tobias.loveridge@gmail.com>
2023-10-28 19:30:36 +1100

    got the kick sequence working.

--- app/beat-maker/page.tsx
+++ app/beat-maker/page.tsx
@@ -2,14 +2,22 @@
 import { ST } from 'next/dist/shared/lib/utils';
 import React, { useState, useEffect, useRef, forwardRef, createRef } from 'react';
 
+
+var stepRefs: React.RefObject<any>[] = [];
+
 const Step = forwardRef<any, any>((props, ref) => {
     const [active, setActive] = useState(false);
 
     useEffect(() => {
+        props.updateStepArray(props.stepID, active);
     }, [active]);
 
+    const handleClick = () => {
+        setActive(!active)
+    }
+
     return(
-        <div ref={ref} className="h-10 bg-stone-700 w-10 m-2 content-center hover:bg-stone-500" onClick={() => {setActive(!active)}}>
+        <div ref={ref} className="h-10 bg-stone-700 w-10 m-2 content-center hover:bg-stone-500" onClick={handleClick}>
             <div className={`h-2 w-2 m-1 ${active ? "bg-green-500" : "bg-white"}`}/>
         </div>
     )
@@ -17,17 +25,24 @@ const Step = forwardRef<any, any>((props, ref) => {
 
 const Kick = (props:any) => {
     var steps = [];
-    var stepRefs: React.RefObject<any>[] = [];
+    //var stepRefs: React.RefObject<any>[] = [];
     const [stepArray, setStepArray] = useState<any>([]);
+    stepRefs = [];
+
+    const updateStepArray = (i:number, isActive:boolean) => {
+        var currentStepArray = props.stepArray;
+        currentStepArray[i] = isActive;
+        props.setStepArray(currentStepArray);
+    }
 
     for (var i = 0; i < 16; i++) {
         let rootRef = createRef<any>();
-        steps.push(<Step ref={rootRef} active={false} key={i} />);
-        stepRefs.push(rootRef);
+
+        steps.push(<Step ref={rootRef} active={false} stepID={i} key={i} updateStepArray={updateStepArray} />);
+        
+        //updateStepArray(i, false);
       }
 
-
-      setStepArray(stepRefs);
 
 
     return(
@@ -55,7 +70,7 @@ export default function BeatMaker(props:any) {
     const [sequenceStep, setSequenceStep] = useState<number>(0);
     const [sequenceOn, setSequenceOn] = useState<boolean>(false);
     const [audioFile, setAudioFile] = useState<any>(new Audio("/Kick.wav"));
-    const [stepArray, setStepArray] = useState<any>([]);
+    const [stepArray, setStepArray] = useState<boolean[]>([]);
 
     const KickStateRef = createRef<any>(); 
 
@@ -67,16 +82,22 @@ export default function BeatMaker(props:any) {
 
     useEffect(() => {
         Sequence();
-    }, [sequenceOn]);
+    }, [sequenceOn, sequenceStep]);
+
+    useEffect(() => {
+        console.log(stepArray);
+    }, [stepArray]);
 
     const Sequence = async () => {
         if(sequenceOn) {
-            audioFile.play();
-            await new Promise(r => setTimeout(r, (30)));
-            document.getElementById(""+ sequenceStep+1)
-            setSequenceStep(sequenceStep+1)
+            if(stepArray[sequenceStep]) {
+                audioFile.play();
+            }
+            await new Promise(r => setTimeout(r, (500)));
+            //document.getElementById(""+ sequenceStep+1)
+            setSequenceStep((sequenceStep+1)%16)
             
-            Sequence();
+            //Sequence();
         }
     }
 
